33f6e3f430c3fffa1d512dbb151152a4b35bb05b
joaoprocopio <joaovitorcprocopio@gmail.com>
2023-08-14 20:06:54 -0300

    part2

--- src/2-semestre/desenvolvimento-web-2/aula-1-ts/index.ts
+++ src/2-semestre/desenvolvimento-web-2/aula-1-ts/index.ts
@@ -1,12 +1,22 @@
-type TOperacao = (a: number, b: number) => number
-type TCallback = (f: TOperacao, a: number, b: number) => number
+// Parte 1
+// type TOperacao = (a: number, b: number) => number
+// type TCallback = (f: TOperacao, a: number, b: number) => number
 
-const callback: TCallback = (f, a, b) => f(a, b)
+// const callback: TCallback = (f, a, b) => f(a, b)
 
-const dif: TOperacao = (a, b) => a - b
-const div: TOperacao = (a, b) => a / b
-const pot: TOperacao = (a, b) => a ** b
+// const dif: TOperacao = (a, b) => a - b
+// const div: TOperacao = (a, b) => a / b
+// const pot: TOperacao = (a, b) => a ** b
 
-console.log("Dif: ", callback(dif, 2, 3))
-console.log("Div: ", callback(div, 2, 3))
-console.log("Pot: ", callback(pot, 2, 3))
+// console.log("Dif: ", callback(dif, 2, 3))
+// console.log("Div: ", callback(div, 2, 3))
+// console.log("Pot: ", callback(pot, 2, 3))
+
+// Parte 2
+const nomes: Array<string> = ["Ana", "Carlos", "Débora", "Lucas"]
+
+// for (const nome of nomes) {
+//   console.log(nome)
+// }
+
+nomes.forEach((nome) => console.log(nome))
