567b93fe233eb2e4c22aa9d6465ef6f82d15b519
joaoprocopio <joaovitorcprocopio@gmail.com>
2023-08-14 20:18:19 -0300

    arrays

--- src/2-semestre/desenvolvimento-web-2/aula-1-ts/2-arrays.ts
+++ src/2-semestre/desenvolvimento-web-2/aula-1-ts/2-arrays.ts
@@ -1,10 +1,6 @@
-// const nomes: Array<string> = ["Ana", "Carlos", "Débora", "Lucas"]
+const nomes: Array<string> = ["Ana", "Carlos", "Débora", "Lucas"]
 
-// for (const nome of nomes) {
-//   console.log(nome)
-// }
-
-// nomes.forEach((nome) => console.log(nome))
+nomes.forEach((nome) => console.log(nome))
 
 const numbers: number[] = [1, 2, 3, 4, 5]
 const double = numbers.map((numero) => numero * 2)
