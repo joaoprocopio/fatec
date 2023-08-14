8a0a06ed83b0b553b390eded121165b5ebbd92bc
joaoprocopio <joaovitorcprocopio@gmail.com>
2023-08-14 19:33:44 -0300

    move

---
+++ src/2-semestre/desenvolvimento-web-2/aula-1-ts/index.ts
@@ -0,0 +1,13 @@
+const sum = (a: number, b: number): number => {
+  return a + b
+}
+
+const sub = (a: number, b: number): number => {
+  return a - b
+}
+
+let result = sum(1, 2)
+console.log(result)
+
+result = sub(5, 4)
+console.log(result)
