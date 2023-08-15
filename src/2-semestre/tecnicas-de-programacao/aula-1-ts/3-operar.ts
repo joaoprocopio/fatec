c196f57dfd66c932e49bb4ddb074d177aed836a0
joaoprocopio <joaovitorcprocopio@gmail.com>
2023-08-14 22:30:56 -0300

    operar 3

---
+++ src/2-semestre/tecnicas-de-programacao/aula-1-ts/3-operar.ts
@@ -0,0 +1,14 @@
+const operar = (v1: number[], v2: string[]) => {
+  const res = []
+
+  for (let i = 0; i < v1.length; i++) {
+    res[i] = v1[i] + v2[i]
+  }
+
+  return res
+}
+
+const v1 = [5, 3, 1, 8, 2]
+const v2 = ["M", "a", "r", "i", "a"]
+
+console.log("Resultado: ", operar(v1, v2))
