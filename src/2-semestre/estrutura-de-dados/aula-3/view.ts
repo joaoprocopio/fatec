3736669d9545eb57ae72b40d08701760a73281e0
joaoprocopio <joaovitorcprocopio@gmail.com>
2023-08-18 21:31:27 -0300

    ex!

--- src/2-semestre/estrutura-de-dados/aula-3/view.ts
+++ src/2-semestre/estrutura-de-dados/aula-3/view.ts
@@ -4,17 +4,23 @@ import { somar, subtrair, despedir } from "./model"
 
 const prompt = promptSync()
 const escolha: string = prompt("Escolha: ")
-const primeiroNumero: number = parseInt(prompt("Digite o primeiro número: "))
-const segundoNumero: number = parseInt(prompt("Digite o segundo número: "))
 
 switch (escolha) {
   case "somar":
-    console.log(somar(primeiroNumero, segundoNumero))
+    const soma1: number = parseInt(prompt("Digite o primeiro número: "))
+    const soma2: number = parseInt(prompt("Digite o segundo número: "))
+
+    console.log(somar(soma1, soma2))
     break
   case "subtrair":
-    console.log(subtrair(primeiroNumero, segundoNumero))
+    const sub1: number = parseInt(prompt("Digite o primeiro número: "))
+    const sub2: number = parseInt(prompt("Digite o segundo número: "))
+
+    console.log(subtrair(sub1, sub2))
     break
   case "despedir":
     console.log(despedir())
     break
 }
+
+export {}
