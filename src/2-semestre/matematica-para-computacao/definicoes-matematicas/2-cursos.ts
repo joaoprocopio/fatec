02a1000974e08b0b0b66e970d9e5467052ace3b9
joaoprocopio <joaovitorcprocopio@gmail.com>
2023-08-24 00:23:02 -0300

    cursos

---
+++ src/2-semestre/matematica-para-computacao/definicoes-matematicas/2-cursos.ts
@@ -0,0 +1,7 @@
+// Efetue uma definição, através de enumeração, de cursos existentes na instituição de ensino em que você está estudando.
+
+enum CursosFatecJacarei {
+  ma = "Meio Ambiente",
+  dsm = "Desenvolvimento de Software Multiplataforma",
+  geo = "Geoprocessamento"
+}
