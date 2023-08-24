9f8f32272af703351306dcf3e883028a500121c8
joaoprocopio <joaovitorcprocopio@gmail.com>
2023-08-24 00:30:22 -0300

    3

---
+++ src/2-semestre/matematica-para-computacao/definicoes-matematicas/3-primos.ts
@@ -0,0 +1,12 @@
+//  Efetue uma definição, através de enumeração, de números primos. Um número é dito primo se só possui como divisor ele mesmo e o número um.
+
+enum NumerosPrimos {
+  Dois = 2,
+  Tres = 3,
+  Cinco = 5,
+  Sete = 7,
+  Onze = 11,
+  Treze = 13
+}
+
+// Imagino que o conjunto de números primos é finito. Portanto, é humanamente impossível escrever linhas de código o suficiente para enumerar todos.
