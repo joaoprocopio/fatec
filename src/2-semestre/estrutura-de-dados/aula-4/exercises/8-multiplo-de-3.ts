bae27d82285df3108ea8679b844413fed8cbc649
joaoprocopio <joaovitorcprocopio@gmail.com>
2023-08-25 19:53:06 -0300

    multiplos de 3

---
+++ src/2-semestre/estrutura-de-dados/aula-4/exercises/8-multiplo-de-3.ts
@@ -0,0 +1,5 @@
+// Escreva um programa, utilizando o bloco for, que imprima todos os múltiplos de 3 contidos entre 0 e 100, inclusive. Os valores devem ser impressos em ordem crescente.
+
+for (let i = 0; i <= 100; i++) {
+  if (i % 3 === 0) console.log(i)
+}
