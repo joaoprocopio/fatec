5b43e04119fbd922dbae7be8b2df62340c63658a
joaoprocopio <joaovitorcprocopio@gmail.com>
2023-08-25 19:16:17 -0300

    aula4

---
+++ src/2-semestre/estrutura-de-dados/aula-4/models.ts
@@ -0,0 +1,9 @@
+export class Cachorro {
+  raca: string
+  constructor(raca: string) {
+    this.raca = raca
+  }
+  latir(): string {
+    return "au au au"
+  }
+}
---
+++ src/2-semestre/estrutura-de-dados/aula-4/views.ts
@@ -0,0 +1,6 @@
+import { Cachorro } from "./models"
+
+const cachorro_1 = new Cachorro("Lhasa")
+
+console.log(cachorro_1.raca)
+console.log(cachorro_1.latir())
