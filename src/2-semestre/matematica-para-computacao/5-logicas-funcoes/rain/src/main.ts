e771611cce53304d0edfcfd799e7cdae4093d1be
joaoprocopio <joaovitorcprocopio@gmail.com>
2023-11-08 21:06:40 -0300

    slowly refactoring

--- src/2-semestre/matematica-para-computacao/5-logicas-funcoes/rain/src/main.ts
+++ src/2-semestre/matematica-para-computacao/5-logicas-funcoes/rain/src/main.ts
@@ -1,7 +1,11 @@
-const velocity_screen = document.getElementById("velocity_screen") as HTMLCanvasElement
-const simulation_context = velocity_screen.getContext("2d") as CanvasRenderingContext2D
+const rainingCanvasEl = document.getElementById("rainingCanvas") as HTMLCanvasElement
+const velocityInputEl = document.getElementById("velocity") as HTMLInputElement
+const angleInputEl = document.getElementById("angle") as HTMLInputElement
+const playButtonEl = document.getElementById("playButton") as HTMLButtonElement
 
-function draw_object(canvas: HTMLCanvasElement, context: CanvasRenderingContext2D, center: number[], radius: number) {
+const context = rainingCanvasEl.getContext("2d") as CanvasRenderingContext2D
+
+const drawObject = (canvas: HTMLCanvasElement, context: CanvasRenderingContext2D, center: number[], radius: number) => {
   const centerX = Math.floor(center[0])
   const centerY = Math.floor(center[1])
   const r = radius
@@ -12,17 +16,14 @@ function draw_object(canvas: HTMLCanvasElement, context: CanvasRenderingContext2
   context.fill()
 }
 
-function update_coord(coords: number[], v: number[], t: number) {
+const updateCoordinates = (coords: number[], v: number[], t: number) => {
   coords[0] = Math.floor(coords[0] + v[0] * t)
   coords[1] = Math.floor(coords[1] + v[1] * t)
 }
 
-const start_button = document.getElementById("start_button") as HTMLButtonElement
-start_button.onclick = function () {
-  const velocity_input = document.getElementById("vel") as HTMLInputElement
-  const velocity = parseFloat(velocity_input.value)
-  const angle_input = document.getElementById("angle") as HTMLInputElement
-  const angle = parseFloat(angle_input.value)
+playButtonEl.onclick = () => {
+  const velocity = parseFloat(velocityInputEl.value)
+  const angle = parseFloat(angleInputEl.value)
   const center: number[] = [0, 0]
   const v: number[] = [0, 0]
   let t: number = 0 // initial time
@@ -31,10 +32,10 @@ start_button.onclick = function () {
   v[0] = velocity * Math.cos((Math.PI * angle) / 180)
   v[1] = velocity * Math.sin((Math.PI * angle) / 180)
 
-  setInterval(function () {
+  setInterval(() => {
     // coordinate update
     t = (t + dt) / dt
-    update_coord(center, v, t)
-    draw_object(velocity_screen, simulation_context, center, r)
+    updateCoordinates(center, v, t)
+    drawObject(rainingCanvasEl, context, center, r)
   }, dt)
 }
