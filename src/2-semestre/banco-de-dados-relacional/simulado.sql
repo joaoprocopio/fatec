d5777e690f3f428c31001613290db88a68f3e089
joaoprocopio <joaovitorcprocopio@gmail.com>
2023-11-24 22:23:56 -0300

    create tables

---
+++ src/2-semestre/banco-de-dados-relacional/simulado.sql
@@ -0,0 +1,37 @@
+CREATE TABLE tbl_departamentos (
+  id_departamento SERIAL,
+  nome_departamento TEXT,
+
+  PRIMARY KEY (id_departamento)
+);
+
+CREATE TABLE tbl_funcionarios (
+  id_funcionario SERIAL PRIMARY KEY,
+  primeiro_nome TEXT,
+  sobrenome TEXT,
+  salario REAL,
+
+  PRIMARY KEY (id_funcionario)
+);
+
+CREATE TABLE tbl_funcionario_departamento (
+  id_funcionario SERIAL,
+  id_departamento INT,
+
+  PRIMARY KEY (id_funcionario, id_departamento),
+
+  FOREIGN KEY (id_funcionario) REFERENCES tbl_funcionarios(id_funcionario),
+  FOREIGN KEY (id_departamento) REFERENCES tbl_departamentos(id_departamento)
+);
+
+CREATE TABLE tbl_tarefas (
+  id_tarefa SERIAL,
+  nome_tarefa TEXT,
+  prazo DATE,
+  id_funcionario INT,
+
+  PRIMARY KEY (id_tarefa),
+
+  FOREIGN KEY (id_funcionario) REFERENCES tbl_funcionarios(id_funcionario)
+);
+
