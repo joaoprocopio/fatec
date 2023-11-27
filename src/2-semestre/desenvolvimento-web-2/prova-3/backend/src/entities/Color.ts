bc08a579ea6b3ae52f4cdab591a9ed6a69e3874f
joaoprocopio <joaovitorcprocopio@gmail.com>
2023-11-27 19:39:49 -0300

    Linter

--- src/2-semestre/desenvolvimento-web-2/prova-3/backend/src/entities/Color.ts
+++ src/2-semestre/desenvolvimento-web-2/prova-3/backend/src/entities/Color.ts
@@ -1,16 +1,16 @@
-import { Entity, PrimaryColumn , Column, PrimaryGeneratedColumn } from "typeorm";
+import { Entity, PrimaryColumn, Column, PrimaryGeneratedColumn } from "typeorm"
 
-@Entity({name:"colors"})
+@Entity({ name: "colors" })
 export class Color {
-    @PrimaryGeneratedColumn()
-    id: number;
+  @PrimaryGeneratedColumn()
+  id: number
 
-    @Column({nullable: false, type:"integer"})
-    red: number;
+  @Column({ nullable: false, type: "integer" })
+  red: number
 
-    @Column({nullable: false, type:"integer"})
-    green: number;
+  @Column({ nullable: false, type: "integer" })
+  green: number
 
-    @Column({nullable: false, type:"integer"})
-    blue: number;
+  @Column({ nullable: false, type: "integer" })
+  blue: number
 }
--- src/2-semestre/desenvolvimento-web-2/prova-3/backend/src/index.ts
+++ src/2-semestre/desenvolvimento-web-2/prova-3/backend/src/index.ts
@@ -1,14 +1,14 @@
-import express from "express";
-import dotenv from "dotenv";
-import cors from "cors";
-import routes from "./routes";
-dotenv.config();
+import express from "express"
+import dotenv from "dotenv"
+import cors from "cors"
+import routes from "./routes"
+dotenv.config()
 
-const PORT = process.env.PORT || 3000;
-const app = express();
+const PORT = process.env.PORT || 3000
+const app = express()
 app.use(cors())
-app.use(express.json());
-app.listen(PORT, () => console.log(`Rodando na porta ${PORT}...`));
+app.use(express.json())
+app.listen(PORT, () => console.log(`Rodando na porta ${PORT}...`))
 
 // define a rota para o pacote /routes
-app.use(routes);
+app.use(routes)
