fa7e81a4dc42e9e366d2273338d611974008bd57
joaoprocopio <joaovitorcprocopio@gmail.com>
2023-11-27 19:35:40 -0300

    vai

--- src/2-semestre/desenvolvimento-web-2/prova-3/backend/src/migrations/1700078592501-default.ts
+++ src/2-semestre/desenvolvimento-web-2/prova-3/backend/src/migrations/1700078592501-default.ts
@@ -1,15 +1,14 @@
-import { MigrationInterface, QueryRunner } from "typeorm"
+import { MigrationInterface, QueryRunner } from "typeorm";
 
 export class Default1700078592501 implements MigrationInterface {
-  name = "Default1700078592501"
+    name = 'Default1700078592501'
 
-  public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(
-      `CREATE TABLE "colors" ("id" integer PRIMARY KEY AUTOINCREMENT NOT NULL, "red" integer NOT NULL, "green" integer NOT NULL, "blue" integer NOT NULL)`
-    )
-  }
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`CREATE TABLE "colors" ("id" integer PRIMARY KEY AUTOINCREMENT NOT NULL, "red" integer NOT NULL, "green" integer NOT NULL, "blue" integer NOT NULL)`);
+    }
 
-  public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`DROP TABLE "colors"`)
-  }
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`DROP TABLE "colors"`);
+    }
+
 }
