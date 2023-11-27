42d4ebf1d17a724e089abd25db07730dae83dd54
joaoprocopio <joaovitorcprocopio@gmail.com>
2023-11-27 19:22:09 -0300

    foi

--- src/2-semestre/desenvolvimento-web-2/prova-3/backend/src/routes/index.ts
+++ src/2-semestre/desenvolvimento-web-2/prova-3/backend/src/routes/index.ts
@@ -1,15 +1,13 @@
-import { Router, Request, Response } from "express";
-import controller from "../controllers/ColorController";
+import { Router, Request, Response } from "express"
+import controller from "../controllers/ColorController"
 
-const routes = Router();
+const routes = Router()
 
-routes.get("/", controller.list);
-routes.get("/:red/:green/:blue", controller.create);
-routes.get("/:id", controller.remove);
+routes.get("/", controller.list)
+routes.get("/:red/:green/:blue", controller.create)
+routes.get("/:id", controller.remove)
 
 //aceita qualquer método HTTP ou URL
-routes.use((_: Request, res: Response) =>
-  res.json({ error: "Requisição desconhecida" })
-);
+routes.use((_: Request, res: Response) => res.json({ error: "Requisição desconhecida" }))
 
-export default routes;
+export default routes
