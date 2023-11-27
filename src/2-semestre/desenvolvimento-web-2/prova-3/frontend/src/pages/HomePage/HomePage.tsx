5d3c1a7cc273d226a96f80f8b812d0e1f253edb2
joaoprocopio <joaovitorcprocopio@gmail.com>
2023-11-27 20:41:01 -0300

    to hex

--- src/2-semestre/desenvolvimento-web-2/prova-3/frontend/src/pages/HomePage/HomePage.tsx
+++ src/2-semestre/desenvolvimento-web-2/prova-3/frontend/src/pages/HomePage/HomePage.tsx
@@ -1,7 +1,24 @@
+import "./HomePage.scss"
+
 import { useColors } from "~/hooks"
+import { toHex } from "~/helpers"
 
 export default function HomePage() {
   const { colors } = useColors()
 
-  return <div>{JSON.stringify(colors, null, 2)}</div>
+  return (
+    <div>
+      <div>
+        {colors.map((color) => {
+          const hexColor = toHex(color)
+
+          return (
+            <div key={color.id} className="color" style={{ backgroundColor: hexColor }}>
+              <p className="color-text">{JSON.stringify(color)}</p>
+            </div>
+          )
+        })}
+      </div>
+    </div>
+  )
 }
