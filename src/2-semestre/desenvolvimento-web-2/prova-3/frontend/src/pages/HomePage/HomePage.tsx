6415f885bb8c2c44d39bf92e84e2bfdeb867ba9b
joaoprocopio <joaovitorcprocopio@gmail.com>
2023-11-27 21:35:01 -0300

    remove cor

--- src/2-semestre/desenvolvimento-web-2/prova-3/frontend/src/pages/HomePage/HomePage.tsx
+++ src/2-semestre/desenvolvimento-web-2/prova-3/frontend/src/pages/HomePage/HomePage.tsx
@@ -3,41 +3,42 @@ import "./HomePage.scss"
 import type { ChangeEventHandler, FormEventHandler } from "react"
 import { useState } from "react"
 
-import { useColors } from "~/hooks"
 import { toHex } from "~/helpers"
+import { useColors } from "~/hooks"
+import type { TBaseColor } from "~/schemas"
 import { ColorsServices } from "~/services"
-import { TBaseColor } from "~/schemas"
 
 export default function HomePage() {
   const { colors, setColors } = useColors()
-  const [color, setColor] = useState<TBaseColor>({
-    red: 0,
-    green: 0,
-    blue: 0
-  })
+  const [color, setColor] = useState<TBaseColor>()
 
-  const handleChange: ChangeEventHandler<HTMLInputElement> = async (event) => {
-    const { name, value } = event.target
-
-    setColor((color) => ({
-      ...color,
-      [name]: value
-    }))
-  }
   const handleBeforeInput: FormEventHandler<HTMLInputElement> = (event) => {
     // Garante que o número colocado esteja entre 0 e 255
     const ValidHexPattern = /^([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$/gm
 
     const value = event.currentTarget.value
     const char = (event as unknown as CompositionEvent).data
-    const valid = ValidHexPattern.test(value.toString() + char)
+    const valid = ValidHexPattern.test(value + char)
 
     if (!valid) return event.preventDefault()
 
     return valid
   }
+
+  const handleChange: ChangeEventHandler<HTMLInputElement> = async (event) => {
+    const { name, value } = event.target
+
+    // @ts-expect-error aq da ruim
+    setColor((color) => ({
+      ...color,
+      [name]: value
+    }))
+  }
+
   const handleSubmit: FormEventHandler<HTMLFormElement> = async (event) => {
     event.preventDefault()
+
+    if (!color) return
 
     const newColor = await ColorsServices.createColor(color)
 
@@ -48,6 +49,9 @@ export default function HomePage() {
     await ColorsServices.removeColor(id)
 
     const index = colors.findIndex((color) => color.id === id)
+
+    if (!index) return setColors([])
+
     const newColors = colors.splice(index, 1)
 
     setColors(newColors)
@@ -56,27 +60,9 @@ export default function HomePage() {
   return (
     <div>
       <form onSubmit={handleSubmit}>
-        <input
-          name="red"
-          placeholder="Vermelho"
-          defaultValue={color.red}
-          onChange={handleChange}
-          onBeforeInput={handleBeforeInput}
-        />
-        <input
-          name="blue"
-          placeholder="Azul"
-          defaultValue={color.blue}
-          onChange={handleChange}
-          onBeforeInput={handleBeforeInput}
-        />
-        <input
-          name="green"
-          placeholder="Verde"
-          defaultValue={color.green}
-          onChange={handleChange}
-          onBeforeInput={handleBeforeInput}
-        />
+        <input name="red" placeholder="Vermelho" onChange={handleChange} onBeforeInput={handleBeforeInput} />
+        <input name="blue" placeholder="Azul" onChange={handleChange} onBeforeInput={handleBeforeInput} />
+        <input name="green" placeholder="Verde" onChange={handleChange} onBeforeInput={handleBeforeInput} />
 
         <button type="submit">Criar</button>
       </form>
