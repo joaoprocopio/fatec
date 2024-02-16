812f193b908cb1781816c0c958b2b39ece617485
joaoprocopio <joaovitorcprocopio@gmail.com>
2024-02-15 22:15:23 -0300

    conecta com o nogo

--- src/3-semestre/desenvolvimento-web-3/aulas/aula-1/index.ts
+++ src/3-semestre/desenvolvimento-web-3/aulas/aula-1/index.ts
@@ -1,13 +1,44 @@
+import cors from "cors"
 import express from "express"
+import helmet from "helmet"
+import mongoose from "mongoose"
 
-const app = express()
+const settings = {
+  port: 3000
+}
 
-app.get("/", (_req, res) => {
-  return res.json({
-    hello: "world"
+const main = async () => {
+  const app = express()
+
+  await mongoose.connect("mongodb://localhost:27017/test")
+
+  app.use(express.json())
+  app.use(express.urlencoded({ extended: true }))
+  app.use(cors())
+  app.use(helmet())
+
+  const UserSchema = new mongoose.Schema({
+    mail: {
+      type: String,
+      maxLength: 50,
+      required: true
+    }
   })
-})
 
-app.listen(3000, () => {
-  console.log("foi")
-})
+  const User = mongoose.model("User", UserSchema)
+
+  const novo = new User({ mail: "asodijasodi" })
+  novo.save()
+
+  app.get("/", async (_req, res) => {
+    const users = await User.find()
+
+    return res.json({ users })
+  })
+
+  app.listen(settings.port, () => {
+    console.log(`servidor rodando na porta ${settings.port}`)
+  })
+}
+
+main()
